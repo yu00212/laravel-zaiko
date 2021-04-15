9bf42773b5415ca728a9ae3762edfae69f3ec232
yusei <yu2000212@icloud.com>
2021-04-15 20:10:29 +0900

    [add] 在庫登録内容確認画面のテンプレートファイルを追加
    在庫登録時、データベース反映前に入力内容の確認をさせる為、入力内容を表示するテンプレートファイルを追加しました。

---
+++ zaikoapp/resources/views/stock/addCheck.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>在庫追加確認</title>
+</head>
+<body>
+
+    <p>下記の内容で間違い無いですか？</p>
+
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <table border=2>
+        <tr><th>購入日</th><td>{{$s['purchase_date']}}</td></tr>
+        <tr><th>期限</th><td> {{$s['deadline']}}</td></tr>
+        <tr><th>商品名</th><td>{{$s['name']}}</td></tr>
+        <tr><th>値段</th><td>{{$s['price']}}</td></tr>
+        <tr><th>在庫数</th><td>{{$s['number']}}</td></tr>
+    </table>
+
+    <form action="/list/addDone">
+        @csrf
+        <input type="hidden" name="purchase_date" value="{{$s['purchase_date']}}">
+        <input type="hidden" name="deadline" value="{{$s['deadline']}}">
+        <input type="hidden" name="name" value="{{$s['name']}}">
+        <input type="hidden" name="price" value="{{$s['price']}}">
+        <input type="hidden" name="number" value="{{$s['number']}}">
+        <input type="submit" value="登録">
+    </form>
+
+
+</body>
+</html>
