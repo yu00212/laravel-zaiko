c40b6f4ce2d13aa47a4ac3c26c90cf2c0850a112
yusei <yu2000212@icloud.com>
2021-05-12 20:11:22 +0900

    [update] ログイン機能日本語化
    ログインとアカウント登録のリンク、エラーメッセージを日本語で表示するよう変更しました。

--- zaikoapp/resources/lang/en/auth.php
+++ zaikoapp/resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '登録した情報と一致しません。再入力してください。',
+    'password' => 'パスワードが正しくありません。',
+    'throttle' => 'ログイン失敗数が多いです。. :seconds 秒後に再度ログインしてください。',
 
 ];
